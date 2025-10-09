46369f2a230feaaf205603399bc0422a9def5c48
komil14 <abdulvokhidovkomiljon@gmail.com>
2025-10-10 05:17:17 +0900

    BRR: initial setup

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+// console.log("Hello World");
+
+// import moment from "moment";
+
+// const currentTime = moment().format("YYYY MM DD");
+// console.log(currentTime);
+
+
+// const person = "Burak";
+// const age = 25;
