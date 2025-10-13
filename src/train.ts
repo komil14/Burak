9f5c9bf191753ca8b3b7eaa6b2d726370760bd47
komil14 <abdulvokhidovkomiljon@gmail.com>
2025-10-13 14:12:52 +0900

    feat: Task H added

--- src/train.ts
+++ src/train.ts
@@ -1,25 +1,32 @@
 // TASK H:
+function positiveInt(arr: number[]): string {
+  const positiveInts = arr.filter(num => num > 0 && Number.isInteger(num));
+  return positiveInts.join('');
+}
+
+console.log(positiveInt([3, -1, 4, 1.5, 2, -3, 5]));
+
 
 
 // TASK G:
-function getHighestIndex(arr: number[]): number {
-  let highest = -Infinity;
-  let highestIndex = -1;
+// function getHighestIndex(arr: number[]): number {
+//   let highest = -Infinity;
+//   let highestIndex = -1;
 
-  arr.forEach((num, index) => {
-    if (num > highest) {
-      highest = num;
-      highestIndex = index;
-    }
-  });
+//   arr.forEach((num, index) => {
+//     if (num > highest) {
+//       highest = num;
+//       highestIndex = index;
+//     }
+//   });
 
-  return highestIndex;
-}
+//   return highestIndex;
+// }
 
-// Test cases
-console.log(getHighestIndex([5, 21, 12, 21, 8])); 
-console.log(getHighestIndex([-10, -20, -5, -15]));
-console.log(getHighestIndex([1, 2, 3, 4, 5]));
+// // Test cases
+// console.log(getHighestIndex([5, 21, 12, 21, 8])); 
+// console.log(getHighestIndex([-10, -20, -5, -15]));
+// console.log(getHighestIndex([1, 2, 3, 4, 5]));
 
 
 // console.log("Hello World");
