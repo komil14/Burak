ff2e7b94958adad3c253aaf0ff4136d527851376
komil14 <abdulvokhidovkomiljon@gmail.com>
2025-10-13 13:30:52 +0900

    fixed minor changes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-console.log('Server is running on port:', process.env.PORT);
-console.log('MongoDB URL:', process.env.MONGO_URL);
+// Cluster => Database => Collection => Documents
