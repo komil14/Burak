1b4750db9ede8e5544cda9b52debe4b6fea4faa3
komil14 <abdulvokhidovkomiljon@gmail.com>
2025-10-13 13:46:28 +0900

    feat: connect Mongodb via mongoose

--- src/server.ts
+++ src/server.ts
@@ -2,3 +2,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Cluster => Database => Collection => Documents
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGO_URL as string,{}).then(
+    (data) => { console.log("Connected to MongoDB");
+    const PORT = process.env.PORT ?? 3000;
+     }
+).catch(
+    (err) => { console.log(err); }  
+); 
