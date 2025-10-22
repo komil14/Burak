87b629888361da26f474e745559b6c7f879b3948
komil14 <abdulvokhidovkomiljon@gmail.com>
2025-10-22 18:58:22 +0900

    fix: server link correction

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ mongoose.connect(process.env.MONGO_URL as string,{}).then(
     const PORT = process.env.PORT ?? 3000;
     app.listen(PORT, () => { 
         console.info(`Server is running on port ${PORT}`);
-        console.info(`http://localhost/:${PORT}/admin\n`); 
+        console.info(`http://localhost:${PORT}/admin\n`); 
     });
      }
 ).catch(
