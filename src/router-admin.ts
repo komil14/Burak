8a0c9cedfd3a0d03d76d43a721e3d9bb0e48f3d5
komil14 <abdulvokhidovkomiljon@gmail.com>
2025-10-30 13:35:12 +0900

    feat: create product controller and product routing apis

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,6 +1,7 @@
 import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
+import productController from "./controllers/product.controller";
 
 
 /* Restaurant */
@@ -19,6 +20,9 @@ routerAdmin
 
 
 /* Product */
+routerAdmin.get("/product/all", productController.getAllProducts);
+routerAdmin.post("/product/create", productController.createNewProduct);
+routerAdmin.post("/product/:id", productController.updateChosenProduct);
 /* User */
 
 
