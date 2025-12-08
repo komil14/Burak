c92ab1912498853038c2eb085b8116d69124e014
komil14 <abdulvokhidovkomiljon@gmail.com>
2025-12-08 17:44:16 +0900

    feat: MIT TASK ADDED

--- src/train.ts
+++ src/train.ts
@@ -1,27 +1,4 @@
-/* 
-VPS =====> Vertual Private Server
-A virtual machine running on a physical server.
-
-VPC =====> Vertual Private Cloud
-A private network inside a cloud platform
-
-*/
-
-import { T } from "./libs/types/common";
-
-/* Project Standards:
--Logging standards(Morgan Format)
-- Naming standards
-    function, method, variable => CAMEL case
-    class => PASCAL case
-    folder => KEBAB case
-    css => SNAKE case
-
--ERROR HANDLING 
-
-*/
-
-/*ZD-TASK:
+/* New task ZD-TASK:
 @MITASK
 */
 
