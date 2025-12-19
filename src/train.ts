7652e0a61ac6a5ecaacdccdf0a31f5a60158506d
komil14 <abdulvokhidovkomiljon@gmail.com>
2025-12-19 19:11:44 +0900

    feat: MIT TASK ADDED

--- src/train.ts
+++ src/train.ts
@@ -1,41 +1,17 @@
-/* 
-VPS =====> Vertual Private Server
-A virtual machine running on a physical server.
+/*
+ZG-TASK:
 
-VPC =====> Vertual Private Cloud
-A private network inside a cloud platform
-
-*/
-
-import { T } from "./libs/types/common";
-
-/* Project Standards:
--Logging standards(Morgan Format)
-- Naming standards
-    function, method, variable => CAMEL case
-    class => PASCAL case
-    folder => KEBAB case
-    css => SNAKE case
-
--ERROR HANDLING 
-
-*/
-
-/*ZF-TASK:
-
-Shunday function yozing, uni string parametri bolsin. String ichidagi har bir sozni bosh harflarini katta harf qilib qaytarsin lekin 1 yoki 2 harfdan iborat sozlarni esa oz holicha qoldirsin.
-MASALAN: capitalizeWords('name should be a string') return 'Name Should be a String'
+Shunday function yozing, u berilgan string parametrni snake casega otkazib qaytarsin. 
+MASALAN: capitalizeWords('name should be a string') return 'name_should_be_a_string'
 
 @MITASK
 */
 
 export function capitalizeWords(input: string): string {
-  return input
-    .split(" ")
-    .map((word) =>
-      word.length > 2 ? word.charAt(0).toUpperCase() + word.slice(1) : word
-    )
-    .join(" ");
-}
+  return input.split(' ').join('_');
+} 
 
-console.log(capitalizeWords("name should be a string"));
+// Test cases
+console.log(capitalizeWords('name should be a string')); 
+console.log(capitalizeWords('hello world'));
+console.log(capitalizeWords('convert this to snake case')); 
