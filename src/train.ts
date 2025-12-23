b404f4bddb50b13c221e2222fe08625edc80c7ac
komil14 <abdulvokhidovkomiljon@gmail.com>
2025-12-24 01:13:41 +0900

    feat: MIT task ADDED

--- src/train.ts
+++ src/train.ts
@@ -1,23 +1,17 @@
 /*
-ZH-TASK:
+TASK ZI
 
-Shunday function yozing, u berilgan array parametrni ichidagi eng katta raqamgacha tushib qolgan raqamlarni hammasini bir array qilib qaytarsin. 
-MASALAN: findDisappearedNumbers([1, 3, 4, 7]) return [2, 5, 6]
+Shundan function yozing, bu function 3 soniydan so'ng
+"Hello World!" so'zini qaytarsin.
 
-@MITASK
+MASALAN: delayHelloWorld("Hello World"); return "Hello World";
 */
 
-function findDisappearedNumbers(nums: number[]): number[] {
-  const maxNum = Math.max(...nums);
-  const numSet = new Set(nums);
-  const disappearedNumbers: number[] = [];
-
-  for (let i = 1; i <= maxNum; i++) {
-    if (!numSet.has(i)) {
-      disappearedNumbers.push(i);
-    }
-  }
-
-  return disappearedNumbers;
-}
-console.log(findDisappearedNumbers([1, 3, 4, 7]));
+function delayHelloWorld(message: string): Promise<string> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(message);
+    }, 3000);
+  });
+} 
+delayHelloWorld("Hello World").then(console.log);
