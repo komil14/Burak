8ba1d78e9c15446de54f2a77d1331a5bf49920af
komil14 <abdulvokhidovkomiljon@gmail.com>
2025-12-26 00:01:01 +0900

    feat: MIT task added

--- src/train.ts
+++ src/train.ts
@@ -1,17 +1,30 @@
 /*
-TASK ZI
+TASK ZJ:
 
-Shundan function yozing, bu function 3 soniydan so'ng
-"Hello World!" so'zini qaytarsin.
+Shunday function yozing, u berilgan array ichidagi
+raqamlarni qiymatini hisoblab qaytarsin.
 
-MASALAN: delayHelloWorld("Hello World"); return "Hello World";
+MASALAN: reduceNestedArray([1, [1, 2, [4]]]); return 8;
 */
 
-function delayHelloWorld(message: string): Promise<string> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(message);
-    }, 3000);
-  });
-} 
-delayHelloWorld("Hello World").then(console.log);
+function reduceNestedArray(arr: any[]): number {
+  let sum = 0;
+
+  function helper(subArr: any[]) {
+    for (const item of subArr) {
+      if (Array.isArray(item)) {
+        helper(item);
+      } else if (typeof item === 'number') {
+        sum += item;
+      }
+    }
+  }
+
+  helper(arr);
+  return sum;
+}
+
+// Misol uchun
+console.log(reduceNestedArray([1, [1, 2, [4]]])); // 8
+console.log(reduceNestedArray([[[5]], 3, [2, [1]]])); // 11
+console.log(reduceNestedArray([10, [20, [30, [40]]]])); // 100
