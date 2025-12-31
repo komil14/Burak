52aa15ee52e3a0ea3e14a530a66e23299ad71f44
komil14 <abdulvokhidovkomiljon@gmail.com>
2025-12-31 16:05:04 +0900

    feat: MIT task added

--- src/train.ts
+++ src/train.ts
@@ -1,19 +1,21 @@
 /*
-ZK-TASK:
+ZL-TASK:
 
-Shunday function yozing, u har soniyada bir marta consolega 1 dan 5 gacha bolgan raqamlarni chop etsin va 5 soniyadan keyin ishini toxtatsin.
-MASALAN: printNumbers()
+Shunday function yozing, u parametrda berilgan stringni kebab casega otkazib qaytarsin. Bosh harflarni kichik harflarga ham otkazsin.
+MASALAN: stringToKebab(“I love Kebab”) return “i-love-kebab”
+
+@MITASK
 */
 
-function printNumbers() {
-    let count = 1;
-    const intervalId = setInterval(() => {
-        console.log(count);
-        if (count === 5) {
-            clearInterval(intervalId);
-        }
-        count++;
-    }, 1000);
+function stringToKebab(str: string): string {
+    return str
+        .toLowerCase() 
+        .trim() 
+        .replace(/\s+/g, '-') 
+        .replace(/[^a-z0-9-]/g, ''); 
 }
 
-printNumbers();
+console.log(stringToKebab("I love Kebab")); 
+console.log(stringToKebab("  Hello World!  "));
+console.log(stringToKebab("MIT task is fun")); 
+
