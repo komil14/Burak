7db8e72412bbb7240ee06f497c0511a096d56e0b
komil14 <abdulvokhidovkomiljon@gmail.com>
2026-01-07 16:10:25 +0900

    feat: MIT TASK ADDED

--- src/train.ts
+++ src/train.ts
@@ -19,3 +19,31 @@ function squareDigits(num: number): string {
 
 console.log(squareDigits(9119)); 
 
+/*
+ZO-TASK
+
+Shunday function yozing, u parametrdagi string ichidagi qavslar miqdori balansda ekanligini aniqlasin. Ya'ni ochish("(") va yopish(")") qavslar soni bir xil bolishi kerak. MASALAN: areParenthesesBalanced("string()ichida(qavslar)soni()balansda") return true.
+
+@MITASK
+*/
+
+function areParenthesesBalanced(str: string): boolean {
+    let balance = 0;
+
+    for (const char of str) {
+        if (char === '(') {
+            balance++;
+        } else if (char === ')') {
+            balance--;
+            if (balance < 0) {
+                return false; 
+            }
+        }
+    }
+
+    return balance === 0;
+}
+
+console.log(areParenthesesBalanced("string()ichida(qavslar)soni()balansda"));
+
+
