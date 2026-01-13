19db7977876960bcef262d08f1cd3d73649be908
komil14 <abdulvokhidovkomiljon@gmail.com>
2026-01-14 06:56:20 +0900

    feat: mIT TASK ADDED

--- src/train.ts
+++ src/train.ts
@@ -1,25 +1,26 @@
 /**
 
-ZP-TASK
+ZQ-TASK
 
-Shunday function yozing, u parametridagi string ichidagi raqam va sonlarni sonini sanasin. MASALAN: countNumberAndLetters("string152%\¥") return {number:3, letter:6}.
+Shunday function yozing, u parametridagi array ichida 2 marta qaytarilgan sonlarni alohida araryda qaytarsin. MASALAN: findDuplicates([1,2,3,4,5,4,3,4]) return [3, 4].
 
 @MITASK
-*/
 
-function countNumberAndLetters(input: string): { number: number; letter: number } {
-    let numberCount = 0;
-    let letterCount = 0;
+ */
 
-    for (const char of input) {
-        if (char >= '0' && char <= '9') {
-            numberCount++;
-        } else if ((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')) {
-            letterCount++;
+function findDuplicates(arr: number[]): number[] {
+    const seen = new Set<number>();
+    const duplicates = new Set<number>();
+
+    for (const num of arr) {
+        if (seen.has(num)) {
+            duplicates.add(num);
+        } else {
+            seen.add(num);
         }
     }
-
-    return { number: numberCount, letter: letterCount };
+    return Array.from(duplicates);
 }
 
-console.log(countNumberAndLetters("string152%\\¥")); // { number: 3, letter: 6 }
+const result = findDuplicates([1, 2, 3, 4, 5, 4, 3, 4]);
+console.log(result);
